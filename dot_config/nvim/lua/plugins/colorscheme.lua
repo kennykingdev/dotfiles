5848e6df471ed220e51f4c23f3d27c9fdb4cce56
Kenny King <16931002+kennykingdev@users.noreply.github.com>
2024-07-13 16:08:30 -0700

    change to tokyonight
    planning to eventually create new tokyonight scheme for flexoki-dark
    colors

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,1 +1,13 @@
-return {}
+return {
+  -- make tokyonight transparent
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
