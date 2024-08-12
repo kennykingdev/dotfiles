ea8f229064c926cec344b003f368079420582f93
Kenny King <16931002+kennykingdev@users.noreply.github.com>
2024-08-12 12:32:54 -0700

    Add support/detection for ansible files

---
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,35 @@
+local util = require("lspconfig.util")
+
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ansiblels = {
+        cmd = { "ansible-language-server", "--stdio" },
+        settings = {
+          ansible = {
+            python = {
+              interpreterPath = "python",
+            },
+            ansible = {
+              path = "ansible",
+            },
+            executionEnvironment = {
+              enabled = false,
+            },
+            validation = {
+              enabled = true,
+              lint = {
+                enabled = true,
+                path = "ansible-lint",
+              },
+            },
+          },
+        },
+        filetypes = { "yaml.ansible" },
+        root_dir = util.root_pattern("ansible.cfg", ".ansible-lint"),
+        single_file_support = true,
+      },
+    },
+  },
+}
