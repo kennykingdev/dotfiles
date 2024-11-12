21b38f135c32bae3e09e143987fa2f475bc80b61
Kenny King <16931002+kennykingdev@users.noreply.github.com>
2024-11-11 20:25:35 -0800

    add neovim config (LazyVim)

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,8 @@
+return {
+  -- add nightfox/carbonfox
+  { "EdenEast/nightfox.nvim" },
+  -- configure LazyVim to load colorscheme
+  { "LazyVim/LazyVim", opts = {
+    colorscheme = "carbonfox",
+  } },
+}
---
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+}
